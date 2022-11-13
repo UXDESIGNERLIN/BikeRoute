876f2e8c8d93f073917157f454ce1a1fd132dcdb
UXDESIGNERLIN <hochialing91@gmail.com>
2022-11-13 16:32:48 +0100

    Added setDirection

--- my-component.js
+++ my-component.js
@@ -9,24 +9,58 @@ export default {
   },
   data() {
     let position = {lat: 51.531795246662604, lng: -0.21797176148605957};
-    return { position} 
+    let map;
+    return { position, map} 
   },
   mounted() {
     this.initMap()
   },
   methods: {
+    setDirections(result, status) {
+      const directionsRenderer = new google.maps.DirectionsRenderer();
+      directionsRenderer.setMap(this.map)
+      //let directionsService = new google.maps.DirectionsService();
+      if (status == google.maps.DirectionsStatus.OK) {
+
+        //display route
+        console.log(result)
+        directionsRenderer.setDirections(result);
+    } else {
+        //delete route from map
+        directionsRenderer.setDirections({ routes: [] });
+      }
+    },
     computePositionForEndpoint(position) {
       console.log('tt', `${position.lng},${position.lat}`, position)
       return `${position.lng},${position.lat}`
       //return position
     },
-    async drawRoute(options) {
-      const data = await this.getLeisureRoutes(options)
-      const {start_latitude ,start_longitude, finish_latitude, finish_longitude} = data
+    async setRoute(options) {
+      const coordinates = await this.getLeisureRoutes(options); 
+      const coordinatesArray = coordinates.split(' ')
+      let arr = []
+      for (const key of coordinatesArray.keys()) {
+        if(arr.length >= 2) {
+          arr = []
+        }
+        arr.push(coordinatesArray[key])
+        if(key%2 !== 0) {
+          this.calculateRoute(arr)
+        }
+      }
+    },
+    async calculateRoute(coordinates) {
+      const [origin, destination] = coordinates
+      let directionsService = new google.maps.DirectionsService();
+      directionsService.route({
+        origin: { lng: parseFloat(origin.split(',')[0]), lat: parseFloat(origin.split(',')[1])}, // Haight.
+        destination: { lng: parseFloat(destination.split(',')[0]), lat: parseFloat(destination.split(',')[1]) }, // Ocean Beach. 
+        travelMode: google.maps.TravelMode.BICYCLING
+      }, this.setDirections)
     },
     async getLeisureRoutes(options) {
      const data = await axios.get(leisureRouteEndpoint(this.computePositionForEndpoint(this.position), options))
-     return data.data.marker[0]['@attributes']
+     return data.data.marker[0]['@attributes'].coordinates
     },
     initMap() {
      const map = new google.maps.Map(document.getElementById('map'), {
@@ -36,6 +70,7 @@ export default {
       });
       const bikeLayer = new google.maps.BicyclingLayer();
       bikeLayer.setMap(map);
+      this.map = map
       const animation = google.maps.Animation.BOUNCE
       const marker = new google.maps.Marker({
         position: {lat: 51.531795246662604, lng: -0.21797176148605957},
@@ -52,5 +87,5 @@ export default {
       });
     },
   },
-  template: `<div><div id='map'></div><leisure-routes @get-leisure-options='drawRoute'/></div>`
+  template: `<div><div id='map'></div><leisure-routes @get-leisure-options='setRoute'/></div>`
 }
